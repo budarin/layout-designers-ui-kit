34169c2d948f7459897208e746324867377bbf44
budarin <budarin-vv@yandex.ru>
2018-11-30 21:34:12 +0300

    [shl] configure git hooks

--- husky.config.js
+++ husky.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     hooks: {
+        'prepare-commit-msg': 'prun pre-commit',
         'pre-commit': 'pretty-quick --staged',
-        'pre-push': 'npm test',
+        'pre-push': 'prun pre-push',
         'post-checkout': 'check-dependencies --install',
         'post-merge': 'check-dependencies --install',
     },
