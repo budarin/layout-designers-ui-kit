df06e8a6fafdc950c8fb6b75cde25f9ce2b56bbc
budarin <budarin-vv@yandex.ru>
2018-12-05 20:26:54 +0300

    [dev] refactoring

--- website/components/SideBar/index.css.d.ts
+++ website/components/SideBar/index.css.d.ts
@@ -2,8 +2,8 @@ export interface IIndexCss {
     readonly locals: {
         readonly sidbar_nav: string;
         readonly sidbarNav: string;
-        readonly title: string;
         readonly active: string;
+        readonly title: string;
     };
     readonly use: Function;
     readonly unuse: Function;
